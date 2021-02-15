8778940da475ad020ee82e4d44493e3e39a7a286
muharrem.sdet <muharrem.sdet@gmail.com>
2021-02-14 23:08:39 -0500

    Live Data added

--- OrderAppKotlinVersion/app/src/main/java/com/example/orderappkotlinversion/MainActivity.kt
+++ OrderAppKotlinVersion/app/src/main/java/com/example/orderappkotlinversion/MainActivity.kt
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.*
-import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,34 +19,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         orderViewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
-        text_view_amount.text = orderViewModel.count.toString()
-        text_view_receipt.text = orderViewModel.text_view_receipt_label
 
         button_increase.setOnClickListener {
-            orderViewModel.count++
-            text_view_amount.text = orderViewModel.count.toString()
+            orderViewModel.onClickButtonIncrease()
         }
 
         button_decrease.setOnClickListener {
-            if (orderViewModel.count > 0) {
-                orderViewModel.count--
-                text_view_amount.text = orderViewModel.count.toString()
-            }
+            orderViewModel.onClickButtonDecrease()
         }
 
         check_box_whipped_cream.setOnCheckedChangeListener { compoundButton: CompoundButton, isChecked: Boolean ->
-            Log.d("Myapp", "Status of checkbox: $isChecked")
-            orderViewModel.whippedCreamChecked = isChecked
+            orderViewModel.onWhippedCreamCheckBox(isChecked)
         }
 
         check_box_chocolate.setOnCheckedChangeListener { compoundButton: CompoundButton, isChecked: Boolean ->
-            Log.d("Myapp", "Status of checkbox: $isChecked")
-            orderViewModel.chocolateChecked = isChecked
+            orderViewModel.onClickChocolateCheckBox(isChecked)
         }
 
         button_order.setOnClickListener {
-            orderViewModel.edit_text_name_label = edit_text_name.text.toString()
-            text_view_receipt.text = orderViewModel.set_receipt_text()
+            Toast.makeText(this, "Order received. Thank you!", Toast.LENGTH_SHORT).show()
         }
+
+        orderViewModel.textViewReceiptLiveData.observe(this, Observer {
+            text_view_receipt.text = it
+        })
+
+        orderViewModel.countLiveData.observe(this, Observer {
+            text_view_amount.text = it.toString()
+        })
+
+        edit_text_name.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                orderViewModel.onEditTextNameChanged(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
     }
 }
