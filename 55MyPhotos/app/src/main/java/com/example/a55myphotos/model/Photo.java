92d2c9e8c50b75ee163fb9a97967b3bd3a779c98
muharrem.sdet <muharrem.sdet@gmail.com>
2021-02-21 17:26:21 -0500

    10.4 Multi-Threading MyPhotos Java version with execute() method

---
+++ 55MyPhotos/app/src/main/java/com/example/a55myphotos/model/Photo.java
@@ -0,0 +1,31 @@
+package com.example.a55myphotos.model;
+
+public class Photo {
+
+	private int albumID;
+	private int id;
+	private String title;
+	private String url;
+	private String thumbnailUrl;
+
+	public int getAlbumID() {
+		return albumID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getThumbnailUrl() {
+		return thumbnailUrl;
+	}
+
+}
