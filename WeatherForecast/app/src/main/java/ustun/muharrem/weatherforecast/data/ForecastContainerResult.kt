6bd8e0efe971995033782914c3cd87b07a5207d7
muharrem.sdet <muharrem.sdet@gmail.com>
2021-03-23 23:40:24 -0400

    Start over

--- WeatherForecast/app/src/main/java/ustun/muharrem/weatherforecast/data/ForecastContainerResult.kt
+++
@@ -1,7 +0,0 @@
-package ustun.muharrem.weatherforecast.data
-
-sealed class ForecastContainerResult{
-    class Success(val forecastContainer: ForecastContainer) : ForecastContainerResult()
-    class Failure(val error: Error) : ForecastContainerResult()
-    object IsLoading : ForecastContainerResult()
-}
