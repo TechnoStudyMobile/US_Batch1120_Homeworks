1ea1247f360d29b2946bd5d67b20cdb0e7d88227
Vlad Kryvovyaz <kidalv85@yahoo.com>
2021-03-27 23:35:51 -0400

    map

--- 9.6_WeatherApplication/app/src/main/java/com/kryvovyaz/a96_weatherapplication/util/Constants.kt
+++ 9.6_WeatherApplication/app/src/main/java/com/kryvovyaz/a96_weatherapplication/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.kryvovyaz.a96_weatherapplication.util
 
-const val WEATHER_API_KEY ="" // go here and register to obtain a KEY https://cloud.google.com/maps-platform/?utm_source=cab&utm_medium=email&utm_campaign=FY21-Q1-global-bypass-email-cs-GMP_scaled_emails
+const val WEATHER_API_KEY = "your key "//https://cloud.google.com/maps-platform/?utm_source=cab&utm_medium=email&utm_campaign=FY21-Q1-global-bypass-email-cs-GMP_scaled_emails
 const val METRIC="M"
 const val US="I"
 //room-database
@@ -9,4 +9,4 @@ const val RESPONSE_PARSING_ERROR_MESSAGE =  "Backend response parsing is  unsucc
 const val DB_IS_EMPTY_ERROR_MASSAGE =  "DB is empty"
 
  const val REQUEST_LOCATION_PERMISSION = 1
- const val PLACES_API_KEY = "AIzaSyBz0KDyN4x75IL87uSiSpZj48zM1pph6nw"
+ const val PLACES_API_KEY = "Your KEY"
